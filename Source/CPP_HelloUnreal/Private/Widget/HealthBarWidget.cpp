e404ac9287c7ef6306f8f79731d3141f975a977e
Seonhyeon Jeon <dookong59@gmail.com>
2026-07-27 16:26:51 +0900

    refact: HUD, Widget 관련 코드 및 에셋 폴더 재정리

--- Source/CPP_HelloUnreal/Private/Widget/HealthBarWidget.cpp
+++ Source/CPP_HelloUnreal/Private/Widget/HealthBarWidget.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Interface/HealthBarWidget.h"
+#include "Widget/HealthBarWidget.h"
 #include "Components/TextBlock.h"
 #include "Components/ProgressBar.h"
 #include "Components/StatComponent.h"
